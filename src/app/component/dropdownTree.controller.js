c4c55d9875ef905fb1b1aae2993eb6fe669fa724
pkempenaers <pieter.kempenaers@gmail.com>
2017-01-29 20:40:35 +0100

    Added: Close on blur

--- src/app/component/dropdownTree.controller.js
+++ src/app/component/dropdownTree.controller.js
@@ -1,10 +1,16 @@
 export default class DropDownTreeController {
 	constructor(
 		$log,
+		$document,
+		$element,
+		$rootScope,
 	) {
 		'ngInject';
 
 		this.$log = $log;
+		this.$document = $document;
+		this.$element = $element;
+		this.$rootScope = $rootScope;
 	}
 
 	$onInit() {
@@ -44,6 +50,33 @@ export default class DropDownTreeController {
 
 	toggleDropdown() {
 		this.open = !this.open;
+		if (this.open) {
+			this.closeToggleOnBlurBinded = this.toggleOnBlur.bind(this);
+			this.$document.on('click', this.closeToggleOnBlurBinded);
+		}
+	}
+
+	toggleOnBlur(e) {
+		if (e.target && e.target.parentElement) {
+			let target = e.target.parentElement;
+			let parentFound = false;
+
+			while (angular.isDefined(target) && target !== null && !parentFound) {
+				if (!!target.className.split && target.className.split(' ').indexOf('dropdown-tree-parent') >= 0 && !parentFound) {
+					if (target === this.$element.children()[0]) {
+						parentFound = true;
+					}
+				}
+				target = target.parentElement;
+			}
+
+			if (!parentFound) {
+				this.$rootScope.$apply(() => {
+					this.toggleDropdown();
+					this.$document.off('click', this.closeToggleOnBlurBinded);
+				});
+			}
+		}
 	}
 
 	optionClicked(option) {
