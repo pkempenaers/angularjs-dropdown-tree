26ee6d36d18945914adbe174d681b88bc12df27a
pkempenaers <pieter.kempenaers@gmail.com>
2017-02-10 23:27:38 +0100

    Added: configurable example settings in docs

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,23 +9,43 @@ export default class MainController {
 		this.options = [
 			{
 				name: 'testoption',
+				label: 'testoption label',
 			},
 			{
 				name: 'test folder',
+				label: 'test folder label',
 				children: [
 					{
 						name: 'test child option',
+						label: 'test child option label',
+					},
+				],
+				childoptions: [
+					{
+						name: 'test child option',
+						label: 'test child option label',
 					},
 				],
 			},
 		];
 
 		this.settings = {
-			selectionLimit: 2,
+			selectionLimit: 0,
+			removeFromFront: true,
+			displayProperty: 'name',
+			childrenProperty: 'children',
+			disableSearch: false,
+			closeOnBlur: true,
+			folderSelectable: true,
 		};
 	}
 
 	selectionChanged(selection) {
 		this.$log.debug(selection);
+	}
+
+	changeSettings() {
+		this.settings.selectionLimit = parseInt(this.settings.selectionLimit, 10);
+		this.settings = angular.extend({}, this.settings);
 	}
 }
